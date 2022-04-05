9b5c7a707dc1e61dfa85413b259786ec75e28036
Ana Priscilla F <64324001+AnaPriscilla@users.noreply.github.com>
2022-04-05 10:01:15 -0300

    adicionado evento com js

---
+++ js/app.js
@@ -0,0 +1,70 @@
+/*Página de Cadastro de Veículos - Informações default - ficticias*/
+function preencherVeiculo() {
+    //Informações do Veículo
+    document.getElementById("placa").setAttribute("value", "ADZ-7145");
+    document.getElementById("renavam").setAttribute("value", "20918475168");
+
+    //Dados do Veículo 
+    document.getElementById("marca").setAttribute("value", "Honda");
+    document.getElementById("modelo").setAttribute("value", "Fit DX 1.4 Flex 16V 5p Mec");
+    document.getElementById("ano").setAttribute("value", "2011");
+    document.getElementById("peso").setAttribute("value", "740 kg");
+    document.getElementById("numeroEixos").setAttribute("value", "5");
+    document.getElementById("numeroChassi").setAttribute("value", "3gA 7r2e93 V9 2c7785");
+    document.getElementById("corVeiculo").setAttribute("value", "Azul");
+    document.getElementById("primeiroLic").setAttribute("value", "15/08/2018");
+    document.getElementById("ultimoLic").setAttribute("value", "08/03/2021");
+}
+
+/*Página de Cadastro de Pessoas - Informações default - ficticias*/
+function preencherPessoa() {
+    //Dados Pessoais
+    document.getElementById("nomeCompleto").setAttribute("value", "Antônia Luciana Catarina da Silva");
+    document.getElementById("dataNascimento").setAttribute("value", "20/08/1992");
+    document.getElementById("localNascimento").setAttribute("value", "Rio de Janiero");
+    document.getElementById("rg").setAttribute("value", "20.138.904-6");
+    document.getElementById("cic").setAttribute("value", "157062");
+    document.getElementById("fone").setAttribute("value", "(75) 99612-3205");
+    document.getElementById("residencia").setAttribute("value", "Feira de Santana");
+    document.getElementById("bairro").setAttribute("value", "Conceição");
+    document.getElementById("cidade").setAttribute("value", "Feira de Santana");
+    document.getElementById("aluguel").setAttribute("value", "870,00");
+    document.getElementById("pensao").setAttribute("value", "2.500,00");
+    document.getElementById("resAnt").setAttribute("value", "Distrito Federal");
+    document.getElementById("cidAnt").setAttribute("value", "Brasília");
+    document.getElementById("cidTempo").setAttribute("value", "Feira de Santana");
+    document.getElementById("localTrabalho").setAttribute("value", "Joana e Caleb Vidros ME");
+    document.getElementById("funcao").setAttribute("value", "Administrativo");
+    document.getElementById("endereco").setAttribute("value", "Rua das Dálias nº 288");
+    document.getElementById("cidEmpresa").setAttribute("value", "Feira de Santana");
+    document.getElementById("foneTrabalho").setAttribute("value", "(75) 3597-4002");
+    document.getElementById("admitidoEm").setAttribute("value", "20/0/2019");
+    document.getElementById("regEmpregador").setAttribute("value", "582.272");
+    document.getElementById("salario").setAttribute("value", "1.805,30");
+    document.getElementById("empregoAnt").setAttribute("value", "Secretaria");
+
+    //Dados do Conjuge
+    document.getElementById("conjuge").setAttribute("value", "Theo Pietro Ramos");
+    document.getElementById("dnConj").setAttribute("value", "14/02/1987");
+    document.getElementById("rgConjuge").setAttribute("value", "45.871.128-7");
+    document.getElementById("localTrabConju").setAttribute("value", "Osvaldo e Diogo Transportes ME");
+    document.getElementById("endConjuge").setAttribute("value", "Rua das Dálias nº 288");
+    document.getElementById("cidConjuge").setAttribute("value", "Feira de Santana");
+    document.getElementById("fConjuge").setAttribute("value", "(75) 99147-0328");
+    document.getElementById("admConjuge").setAttribute("value", "02/05/2010");
+    document.getElementById("refConjuge").setAttribute("value", "(75) 3866-2867");
+    document.getElementById("salarioConjuge").setAttribute("value", "3.805,94");
+
+    //Dados de Referência
+    document.getElementById("parente").setAttribute("value", "Irmã");
+    document.getElementById("nomeParente").setAttribute("value", "Brenda Lúcia Juliana Santos");
+    document.getElementById("foneParente").setAttribute("value", "(75) 99893-9257");
+    document.getElementById("ruaParente").setAttribute("value", "Avenida Feitoria");
+    document.getElementById("bairroParente").setAttribute("value", "Santo André");
+    document.getElementById("cidadeParente").setAttribute("value", "Feira de Santana");
+    document.getElementById("nomeAmigo").setAttribute("value", "Eduardo Kaique Ferreira");
+    document.getElementById("foneAmigo").setAttribute("value", "(75) 98876-5571");
+    document.getElementById("ruaAmigo").setAttribute("value", "Alameda Santos Dumont");
+    document.getElementById("bairroAmigo").setAttribute("value", "Rua Girassol");
+    document.getElementById("cidadeAmigo").setAttribute("value", "Feira de Santana");
+}
